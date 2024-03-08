6f36a73c34309790f9cb9db64d66944598685a9c
nursebil canbolat <nursebiltw@gmail.com>
2024-03-08 23:57:32 +0300

    nodemon deneme

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const fn = function () {
-  console.log('Hello World');
-};
+const express = require('express');
+const app = express();
 
-fn();
+app.get('/',(req,res)=>{
+  const photo = {
+    id:1,
+    name:"Photo Name",
+    description:"Photo Description"
+  };
+  res.send(photo);
+});
+
+const port=3000;
+app.listen(port,()=>{
+  console.log(`Sunucu ${port} portunda baslatildi..`);
+});
