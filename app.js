0ca88376688b8715f6094a2634393b22b38c350b
nursebil canbolat <nursebiltw@gmail.com>
2024-03-10 13:57:02 +0300

    Ejs files uploaded

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const app = express();
+const ejs = require('ejs');
 const path = require('path');
+
+const app = express();
+
+// TEMPLATE ENGINE
+app.set('view engine', ejs);
 
 // MIDDLEWARES
 app.use(express.static('public'));
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index.ejs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add.ejs');
 });
 
 const port = 3000;
